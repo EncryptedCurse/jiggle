6c3fcc828bf40343fdf51e78a8fb65da76c67d8a
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2021-08-05 01:34:35 -0400

    initial commit

---
+++ Jiggle/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using Timer = System.Timers.Timer;
+using System.Windows.Forms;
+using Jiggle.Properties;
+
+namespace Jiggle {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AppContext());
+        }
+    }
+
+    public class AppContext : ApplicationContext {
+        private Timer timer;
+        private NotifyIcon trayIcon;
+        private ContextMenu contextMenu;
+        private MenuItem menuToggle, menuExit;
+        private Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
+        private Random random = new Random();
+
+        public AppContext() {
+            timer = new Timer(2.5 * 60 * 1000);
+            timer.Elapsed += MoveCursor;
+
+            menuToggle = new MenuItem("Enabled", Toggle);
+            menuExit = new MenuItem("Exit", Exit);
+            contextMenu = new ContextMenu(new MenuItem[] { menuToggle, menuExit });
+            trayIcon = new NotifyIcon() {
+                Icon = Resources.AppIcon,
+                ContextMenu = contextMenu,
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e) {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void Toggle(object sender, EventArgs e) {
+            menuToggle.Checked = !menuToggle.Checked;
+            if (menuToggle.Checked) {
+                timer.Start();
+            } else {
+                timer.Stop();
+            }
+        }
+
+        void MoveCursor(object source, ElapsedEventArgs e) {
+            int randomX = random.Next(screenBounds.Width);
+            int randomY = random.Next(screenBounds.Height);
+            Cursor.Position = new Point(randomX, randomY);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# jiggle
+
+Simple Windows tray utility that will move your cursor to a random position every 2.5 minutes.
+
+Due to legal reasons, I have been advised to continue this README no further.
