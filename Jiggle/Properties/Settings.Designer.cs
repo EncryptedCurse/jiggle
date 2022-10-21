6432dbfd0929e5051de8616e723bea9c841993ce
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2022-10-21 13:32:58 -0400

    update default settings

--- Jiggle/Properties/Settings.Designer.cs
+++ Jiggle/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace Jiggle.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("startMenu")]
+        [global::System.Configuration.DefaultSettingValueAttribute("mouseJump")]
         public string Mode {
             get {
                 return ((string)(this["Mode"]));
@@ -61,7 +61,7 @@ namespace Jiggle.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("60")]
         public uint ActivityInterval {
             get {
                 return ((uint)(this["ActivityInterval"]));
@@ -73,7 +73,7 @@ namespace Jiggle.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("20")]
         public uint PauseInterval {
             get {
                 return ((uint)(this["PauseInterval"]));
